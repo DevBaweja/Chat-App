08f6580751b1f7c5da32a856e8b81550c0d8d42c
unknown <18ucs081@lnmiit.ac.in>
2020-08-06 11:37:22 +0530

    Changed Dark Theme

--- client/src/js/views/header.view.js
+++ client/src/js/views/header.view.js
@@ -51,8 +51,29 @@ export const renderUser = () => {
         </button>
     </form>
     `;
-
     document.querySelector(elements.Header).insertAdjacentHTML('beforeend', markup);
+
+    const menu__item = item => `
+    <li class="menu__item">
+        <div class="menu__link" role="button" data-goTo="${item.toLowerCase()}" title="${item}">
+            ${item}
+        </div>
+    </li>
+    `;
+
+    markup = `
+    <div class="menu">
+        <ul class="menu__list">
+            <li class="menu__item">
+                <div class="menu__link menu__link--active" role="button" data-goTo="chats">
+                    Chats
+                </div>
+            </li>
+            ${['Favourites', 'Active', 'Friends', 'Groups'].map(cur => menu__item(cur)).join('')}
+        </ul>
+    </div>`;
+
+    // document.querySelector(elements.Header).insertAdjacentHTML('beforeend', markup);
 
     markup = `
     <div class="about-me">
