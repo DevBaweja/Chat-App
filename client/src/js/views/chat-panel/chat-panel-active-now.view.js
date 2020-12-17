79c90372f6cd35667ef4c403fefcc5b13d051800
Dev Baweja <devbaweja576@gmail.com>
2020-12-18 04:48:07 +0530

    Online Active now

--- client/src/js/views/chat-panel/chat-panel-active-now.view.js
+++ client/src/js/views/chat-panel/chat-panel-active-now.view.js
@@ -1,21 +1,30 @@
-import { elements, select } from '../../utils/base.util';
+import { elements, select, mode } from '../../utils/base.util';
 import { renderTitle, renderItem, extractData } from '../chat-panel.view';
 
 export const renderActiveNow = ({ data }, user) => {
     // Parse Data
     const parseData = extractData(data, user);
+
+    const onlineData = parseData.reduce((acc, item) => {
+        switch (item.status) {
+            case mode.status.online:
+                acc.push({ ...item });
+        }
+        return acc;
+    }, []);
+
     const className = 'active-now';
 
     const title = {
         label: 'Active Now',
-        count: parseData.length,
+        count: onlineData.length,
     };
 
     const markup = `
     <div class="chat-panel-active-now">     
         ${renderTitle(title, className)}
         <ul class="chat-panel-active-now__list">
-            ${parseData.map(item => renderItem(item, className)).join('')}
+            ${onlineData.map(item => renderItem(item, className)).join('')}
         </ul>
     </div>
     `;
