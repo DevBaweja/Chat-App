99d4c7ae0972a0bce2fdf37f85e1bbb071a3278e
Dev Baweja <18ucs081@lnmiit.ac.in>
2020-12-20 12:59:45 +0530

    Fractal Tree Movement and Clicks

--- client/dist/animate/fractal-tree/sketch.js
+++ client/dist/animate/fractal-tree/sketch.js
@@ -1,12 +1,22 @@
-const angle = 25;
+let angle;
+let length;
+let rate;
+let clicked = false;
+const minAngle = 15;
+const minLength = 75;
+const maxAngle = 40;
+const maxLength = 100;
 const weight = 8;
-const length = 90;
-const minLength = 4;
+const minWeight = 4;
 const lengthFactor = 3 / 4;
 const weightFactor = 2 / 3;
 
 function setup() {
     createCanvas(640, 360);
+    angle = minAngle;
+    length = minLength;
+    rate = +1;
+    clicked = false;
 }
 
 function draw() {
@@ -14,7 +24,23 @@ function draw() {
     stroke(attribute['color']);
     translate(width / 2, height);
     angleMode(DEGREES);
+    frameRate(10);
+    angle += rate;
+    length += rate;
+    if (angle == maxAngle && length == maxLength) rate = -1;
+    if (angle == minAngle && length == minLength) rate = +1;
+
     branch(length, weight);
+}
+
+function mouseClicked() {
+    if (!clicked) {
+        noLoop();
+        clicked = true;
+    } else {
+        loop();
+        clicked = false;
+    }
 }
 
 const branch = (size, width) => {
@@ -22,7 +48,7 @@ const branch = (size, width) => {
     line(0, 0, 0, -size);
     translate(0, -size);
 
-    if (size > minLength) {
+    if (size > minWeight) {
         push();
         rotate(angle);
         branch(size * lengthFactor, width * weightFactor);
