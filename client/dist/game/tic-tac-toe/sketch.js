82cb6d9a5f85f7d027a9fcc410be35c301bb84a9
Dev Baweja <18ucs081@lnmiit.ac.in>
2020-12-27 09:59:28 +0530

    Tic Tac Toe without AI

--- client/dist/game/tic-tac-toe/sketch.js
+++ client/dist/game/tic-tac-toe/sketch.js
@@ -5,62 +5,112 @@ const size = 3;
 let done;
 let currentPlayer;
 let h, w;
+let available;
 
 function setup() {
     createCanvas(500, 500);
-    board = Array(size)
-        .fill()
-        .map(() => Array(size).fill(''));
-
-    currentPlayer = playerX;
     done = false;
+    board = [];
+    for (let x = 0; x < size; x++) {
+        board[x] = [];
+        for (let y = 0; y < size; y++) {
+            board[x][y] = '';
+        }
+    }
+    currentPlayer = playerX;
+    calculateAvailable();
 }
 
+const calculateAvailable = () => {
+    // Recalculating available
+    available = [];
+    for (var x = 0; x < size; x++)
+        for (var y = 0; y < size; y++) {
+            if (!board[x][y]) available.push({ x, y });
+        }
+};
+
+const changePlayer = () => (currentPlayer = currentPlayer == playerX ? playerY : playerX);
+
+const equal = (a, b, c) => a == b && b == c && c == a && a != '';
+
+const checkWinner = () => {
+    let winner;
+    // Horizontal
+    for (var i = 0; i < size; i++) {
+        if (equal(board[i][0], board[i][1], board[i][2])) {
+            winner = board[i][0];
+        }
+    }
+    // Vertical
+    for (var j = 0; j < size; j++) {
+        if (equal(board[0][j], board[1][j], board[2][j])) {
+            winner = board[0][j];
+        }
+    }
+
+    // Diagonal
+    if (equal(board[0][0], board[1][1], board[2][2])) {
+        winner = board[1][1];
+    }
+    if (equal(board[2][0], board[1][1], board[0][2])) {
+        winner = board[1][1];
+    }
+    // No winner and no space available
+    if (!winner && !available.length) return 'Tie';
+    if (winner) return winner;
+};
+
+const nextTurn = () => {
+    // If available space then choose randomly and place it
+    if (available.length != 0) {
+        let spot = random(available);
+        let { x, y } = spot;
+
+        board[x][y] = playerY;
+        // Change player
+        changePlayer();
+        // Recalculating available
+        calculateAvailable();
+    }
+};
 function mousePressed() {
-    if (!done) {
-        if (currentPlayer == playerX) {
-            var i = floor(mouseX / h);
-            var j = floor(mouseY / w);
-            console.log({ i, j });
+    if (done) return;
+    switch (currentPlayer) {
+        case playerX:
+            {
+                let x = floor(mouseY / h);
+                let y = floor(mouseX / w);
 
-            var isAvailable = false;
-            for (var x = 0; x < available.length; x++) {
-                var findA = available[x];
-                if (findA[0] == i && findA[1] == j) isAvailable = true;
+                let index = available.findIndex(spot => spot.x == x && spot.y == y);
+                if (index != -1) {
+                    board[x][y] = playerX;
+                    currentPlayer = playerY;
+                    available.splice(index, 1);
+                }
             }
-            // Using find,indexOf,includes functions of js
-            if (isAvailable) {
-                board[i][j] = player[currentPlayer];
-                // Change player
-                changePlayer();
-                // Recalculating available
-                calculateAvailable();
+            break;
+        case playerY:
+            {
+                nextTurn();
             }
-        } else nextTurn();
-        //  whoIsWinner = whoIsWinner ? false : true;
+            break;
+    }
 
-        // Check winner
-        var result = checkWinner();
-        if (result != null) {
-            //createP(result).style('color','#000').style('font-size','32pt');
-            switch (player.indexOf(result)) {
-                case 0:
-                    win++;
-                    break;
-                case 1:
-                    lose++;
-                    break;
-                case -1:
-                    tie++;
-                    break;
-            }
-            console.log(win, tie, lose);
-            console.log(result);
-            // To stop mousePressed in this 5 sec interval
-            done = true;
-            // Winner Line
-            //drawWinnerLine();
-            setTimeout(setup, 5000);
+    // Check winner
+    var result = checkWinner();
+    if (result) done = true;
+    switch (result) {
+        case 'Tie':
+            console.log('Tie');
+            break;
+        case playerX:
+            console.log('X');
+            break;
+        case playerY:
+            console.log('Y');
+            break;
+        default: {
         }
     }
 }
